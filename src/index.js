e4e0e949f11e6ae70b74e281b9977834617bbedb
guqianfeng <guqianfeng@hudunsoft.com>
2020-07-29 09:22:43 +0800

    babel-loader配置

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log('hello world')
+// require('./test')
+// console.log('hello world')
+
+// for (let i of [1,2,3]) {
+//   console.log(i)
+// }
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,22 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  mode: "development",
+  optimization: {
+    minimize: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
   }
 };
