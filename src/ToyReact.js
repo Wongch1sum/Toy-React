f113bd12e659b6bca20a5cf537cb543a28c1e914
guqianfeng <guqianfeng@hudunsoft.com>
2020-07-30 09:26:09 +0800

    渲染字符串

--- src/ToyReact.js
+++ src/ToyReact.js
@@ -1,5 +1,17 @@
 export const ToyReact = {
-  createElement () {
+  createElement (type, attrs, ...children) {
     console.log(arguments)
+    let element
+    if (typeof type === 'string')
+      element = document.createElement(type)
+    for (let attr in attrs) {
+      element.setAttribute(attr, attrs[attr])
+    }
+    for (let child of children) {
+      if (typeof child === 'string')
+        child = document.createTextNode(child)
+      element.appendChild(child)
+    }  
+    return element   
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import {ToyReact} from './ToyReact'
 class MyComponent {
 
 }
-const a = <MyComponent id="my-conponent" test="component-test"/>
 
+// const a = <MyComponent id="my-conponent" test="component-test"/>
 
 const b = (
   <div id="my-div" test="div-test">
@@ -13,3 +13,5 @@ const b = (
     <span>!!!</span>
   </div>
 )
+console.log(b)
+document.body.appendChild(b)
