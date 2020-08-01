a30209f2190f751655c031c19c1ab00f62e068fb
qianfengg <fred_gqf0221@163.com>
2020-08-01 11:48:06 +0800

    热更新

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,10 +1,15 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: './dist',
+    open: true
   },
   mode: "development",
   optimization: {
@@ -27,5 +32,10 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html')
+    })
+  ]
 };
